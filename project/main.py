d1a3619a8dbe98c4939ca7f139117f0db5442cee
maxim.fomichev <somebody.max@yandex.ru>
2024-06-11 22:42:00 +0300

    Алгоритм анонимизации данных на результирующем jpg.
    Представлено первое приближение реализации алгоритма по анонимизации персональных данных на финальной картинке.
    
    Исправлены мердж-конфликты

--- project/main.py
+++
@@ -1,9 +0,0 @@
-from image_anonymizer import anonymize_image
-
-if __name__ == "__main__":
-    input_image_path = "input.jpg"
-    output_image_path = "output.jpg"
-    words_to_anonymize = ["Рак", "14.04.2024", "Гематокрит"]
-
-    anonymize_image(input_image_path, words_to_anonymize, output_image_path)
-    print("Anonymization complete. Check the output image.")
