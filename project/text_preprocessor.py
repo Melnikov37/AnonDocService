e8e610473f146837218794aad39c68a9364b6a9e
Maxim Fomichev <maksim.fomichev@altezza.org>
2024-06-14 12:35:26 +0300

    Улучшено распознавание персональных данных.
    Основное изменение - добавление фильтрации к сущностям, найденным с помощью регулярных выражений и модели nlp фильтрации по контексту. Формируется словарь контекстных слов для персональных данных по каждой группе NER. В будущем контексные подсказки можно расширить до более крупного датасета.

--- project/text_preprocessor.py
+++ project/text_preprocessor.py
@@ -15,13 +15,27 @@ def extract_sentences_by_newline(text):
 def preprocess(text, lang='russian'):
     rows = extract_sentences_by_newline(text)
     cleaned_rows = []
+
+    date_pattern = re.compile(r'\b\d{1,2}\.\d{1,2}\.\d{4}\b')
+
     for row in rows:
-        sentence = row
-        sentence = re.sub(r'[^\w\s]', '', sentence)
-        tokens = nltk.word_tokenize(sentence, lang)
+        dates = date_pattern.findall(row)
+
+        for i, date in enumerate(dates):
+            row = row.replace(date, f"__DATE{i}__")
+
+        row = re.sub(r'[^\w\s]', '', row)
+
+        for i, date in enumerate(dates):
+            row = row.replace(date, f"__DATE{i}__")
+
+        tokens = nltk.word_tokenize(row, lang)
         filtered_tokens = [word.lower() for word in tokens if not word in stopwords.words(lang)]
+
+        for i, date in enumerate(dates):
+            filtered_tokens = [date if token == f"__date{i}__" else token for token in filtered_tokens]
+
         filter_sentence = filtered_tokens
         cleaned_rows.append(filter_sentence)
 
     return cleaned_rows
-
